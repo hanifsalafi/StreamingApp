b278b81f9c3acfd5b488a054c5335f3d0de0100b
Hanif Salafi <hanifsalafi456@gmail.com>
2018-09-21 17:32:41 +0700

    Update: splash,link streaming

--- app/src/main/java/com/example/asus/streamingapp/MainActivity.kt
+++ app/src/main/java/com/example/asus/streamingapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.asus.streamingapp
 
+import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.net.Uri
+import android.os.Handler
 import android.view.View
 import android.widget.MediaController
 import android.widget.TextView
@@ -11,10 +13,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var playbackPosition = 0
-    private val Url = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov"
+//    private var playbackPosition = 0
+    private val Url = "http://ott.tvri.co.id/Content/HLS/Live/Channel(TVRINASIONAL)/index.m3u8"
 
     private lateinit var mediaController : MediaController
+
+    private val SPLASH_TIME_OUT = 4000;
+
+    private lateinit var Handler : Handler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,40 +28,49 @@ class MainActivity : AppCompatActivity() {
 
         mediaController = MediaController(this)
 
-        videoView.setOnPreparedListener{
-            mediaController.setAnchorView(videoContainer)
-            videoView.setMediaController(mediaController)
-            videoView.seekTo(playbackPosition)
-            videoView.start()
-        }
+        videoView.setVideoPath(Url)
+        videoView.requestFocus()
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+        videoView.start()
 
-        videoView.setOnInfoListener { player, what, extras ->
-            if(what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START)
-                progressBar.visibility = View.INVISIBLE
-            true
-        }
+//      mediaController = MediaController(this)
+
+//        videoView.setOnPreparedListener{
+//            mediaController.setAnchorView(videoContainer)
+//            videoView.setMediaController(mediaController)
+//            videoView.seekTo(playbackPosition)
+//            videoView.start()
+//        }
+//
+//        videoView.setOnInfoListener { player, what, extras ->
+//            if(what == MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START)
+//                progressBar.visibility = View.INVISIBLE
+//            true
+//        }
 
         txtMarquee.isSelected = true
     }
 
-    override fun onStart(){
-        super.onStart()
-
-        var uri = Uri.parse(Url)
-        videoView.setVideoURI(uri)
-        progressBar.visibility = View.VISIBLE
-    }
-
-    override fun onPause() {
-        super.onPause()
-
-        videoView.pause()
-        playbackPosition = videoView.currentPosition
-    }
-
-    override fun onStop(){
-        videoView.stopPlayback()
-
-        super.onStop()
-    }
+//
+//    override fun onStart(){
+//        super.onStart()
+//
+//        var uri = Uri.parse(Url)
+//        videoView.setVideoURI(uri)
+//        progressBar.visibility = View.VISIBLE
+//    }
+//
+//    override fun onPause() {
+//        super.onPause()
+//
+//        videoView.pause()
+//        playbackPosition = videoView.currentPosition
+//    }
+//
+//    override fun onStop(){
+//        videoView.stopPlayback()
+//
+//        super.onStop()
+//    }
 }
---
+++ app/src/main/java/com/example/asus/streamingapp/SplashScreenActivity.kt
@@ -0,0 +1,28 @@
+package com.example.asus.streamingapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val background = object : Thread(){
+            override fun run() {
+                try {
+                    Thread.sleep(3000)
+
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+}
